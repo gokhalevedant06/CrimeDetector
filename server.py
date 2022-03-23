9c439b141bc9a83f9fd8e0b3e22e79253fae610f
AryanShinde <aryan.shinde20@vit.edu>
2022-03-23 16:36:53 +0530

    initial commit

--- server.py
+++ server.py
@@ -1,6 +1,4 @@
-from flask import Flask
 
-app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
@@ -9,3 +7,7 @@ def hello_world():
 @app.route("/<string:pagename>")
 def html_page(pagename  = None):
     return f"hii {pagename}"
+@app.route("/test")
+def okay():
+    db.todos.insert_one({'title': "todo title", 'body': "todo body"})
+    return flask.jsonify(message="success") 
