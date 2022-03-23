ac303db48e9a2b9dba216a46a08e76011c8859a6
shivam256 <shivamgavandi256@gmail.com>
2022-03-23 17:19:33 +0530

    flask testing

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
@@ -6,8 +7,17 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/test")
+def test_route():
+    a = requests.get("https://v2.jokeapi.dev/joke/Any")
+    # print(a.json())
+    data = a.json()
+    print(data)
+    return "success";
+
 @app.route("/<string:pagename>")
 def html_page(pagename  = None):
     return f"hii {pagename}"
 
-    
+
+
